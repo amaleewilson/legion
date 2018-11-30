ac508eef065eb101e69a930b8ae5309a4b98d1a7
Amalee Wilson <amaleewilson@lanl.gov>
2018-11-30 11:37:58 -0700

    update for viz

--- test/realm/data_transfer/data/viz.py
+++ test/realm/data_transfer/data/viz.py
@@ -12,22 +12,41 @@ import matplotlib.pylab as plt2
 dfs = []
 
 # dfs.append(pd.read_csv('modified_og_transpose.csv'))
-dfs.append(pd.read_csv('perfdata_kernel_transpose1.csv'))		
-dfs.append(pd.read_csv('modified_shared_transpose.csv'))		
+# dfs.append(pd.read_csv('perfdata_kernel_transpose1.csv'))		
+# dfs.append(pd.read_csv('modified_shared_transpose.csv'))		
 # dfs.append(pd.read_csv('perfdata_kernel_transpose2.csv'))
-dfs.append(pd.read_csv('perfdata_kernel_share_transpose.csv'))	
+# dfs.append(pd.read_csv('perfdata_kernel_share_transpose.csv'))	
 # dfs.append(pd.read_csv('perfdata_kernel_no_transpose.csv'))		
 # dfs.append(pd.read_csv('perfdata_memcpy_no_transpose.csv'))	
 # dfs.append(pd.read_csv('perfdata_memcpy_transpose1.csv'))
+# dfs.append(pd.read_csv('shared_mem_vs_non.csv'))
+
+dfs.append(pd.read_csv('perfdata_kernel_transpose201811301108.csv'))
+dfs.append(pd.read_csv('perfdata_kernel_transpose201811301118.csv'))
+
 
 
 df = pd.concat(dfs, ignore_index=True, sort=False)
 df["mb"] = round(df["bytes"]/1e+6,3); 
 df["gb/s"] = df["bytesPerNano"]; 
-df["note"] = "mod has four fields"; 
 
-g = sns.factorplot("mb", col="note", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
+g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
 
 g.set_xticklabels(rotation=45)
+# g.fig.get_axes()[0].set_yscale('log')
+plt.show()
 
+dfs = []
+
+dfs.append(pd.read_csv('perfdata_kernel_transpose201811301122.csv'))
+dfs.append(pd.read_csv('perfdata_kernel_transpose201811301120.csv'))
+
+df = pd.concat(dfs, ignore_index=True, sort=False)
+df["mb"] = round(df["bytes"]/1e+6,3); 
+df["gb/s"] = df["bytesPerNano"]; 
+
+g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
+
+g.set_xticklabels(rotation=45)
+# g.fig.get_axes()[0].set_yscale('log')
 plt.show()
