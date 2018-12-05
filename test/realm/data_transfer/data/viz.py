f9d0cc20466d5ee14f2be1f2d3f4e4c058f10ec7
Amalee Wilson <amaleewilson@lanl.gov>
2018-12-04 18:52:56 -0700

    added graph and script

--- test/realm/data_transfer/data/viz.py
+++ test/realm/data_transfer/data/viz.py
@@ -21,32 +21,35 @@ dfs = []
 # dfs.append(pd.read_csv('perfdata_memcpy_transpose1.csv'))
 # dfs.append(pd.read_csv('shared_mem_vs_non.csv'))
 
-dfs.append(pd.read_csv('perfdata_kernel_transpose201811301108.csv'))
-dfs.append(pd.read_csv('perfdata_kernel_transpose201811301118.csv'))
+dfs.append(pd.read_csv('data_201812041831.csv'))
 
 
 
 df = pd.concat(dfs, ignore_index=True, sort=False)
-df["mb"] = round(df["bytes"]/1e+6,3); 
+df["mb"] = round(df["bytes"]/1e+6,1); 
 df["gb/s"] = df["bytesPerNano"]; 
 
-g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
+# g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
 
-g.set_xticklabels(rotation=45)
+g = sns.barplot(x="mb", y="gb/s", hue="method", data=df[df.bytes > 131072])
+
+g.set(ylim=(df[df.bytes > 131072]["gb/s"].min() - 1,df[df.bytes > 131072]["gb/s"].max() + 1))
+
+# g.set_xticklabels(rotation=45)
 # g.fig.get_axes()[0].set_yscale('log')
 plt.show()
 
-dfs = []
+# dfs = []
 
-dfs.append(pd.read_csv('perfdata_kernel_transpose201811301122.csv'))
-dfs.append(pd.read_csv('perfdata_kernel_transpose201811301120.csv'))
+# dfs.append(pd.read_csv('perfdata_kernel_transpose201811301122.csv'))
+# dfs.append(pd.read_csv('perfdata_kernel_transpose201811301120.csv'))
 
-df = pd.concat(dfs, ignore_index=True, sort=False)
-df["mb"] = round(df["bytes"]/1e+6,3); 
-df["gb/s"] = df["bytesPerNano"]; 
+# df = pd.concat(dfs, ignore_index=True, sort=False)
+# df["mb"] = round(df["bytes"]/1e+6,3); 
+# df["gb/s"] = df["bytesPerNano"]; 
 
-g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
+# g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
 
-g.set_xticklabels(rotation=45)
+# g.set_xticklabels(rotation=45)
 # g.fig.get_axes()[0].set_yscale('log')
-plt.show()
+# plt.show()
