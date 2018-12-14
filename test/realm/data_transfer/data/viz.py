994b1c28cee253608b3933fd422cb0f5c77aa693
Amalee Wilson <amaleewilson@lanl.gov>
2018-12-14 12:21:39 -0700

    graphs

--- test/realm/data_transfer/data/viz.py
+++ test/realm/data_transfer/data/viz.py
@@ -21,23 +21,66 @@ dfs = []
 # dfs.append(pd.read_csv('perfdata_memcpy_transpose1.csv'))
 # dfs.append(pd.read_csv('shared_mem_vs_non.csv'))
 
-dfs.append(pd.read_csv('data_201812041831.csv'))
+dfs.append(pd.read_csv('data_201812122234.csv'))
 
 
 
 df = pd.concat(dfs, ignore_index=True, sort=False)
 df["mb"] = round(df["bytes"]/1e+6,1); 
-df["gb/s"] = df["bytesPerNano"]; 
+df["gb/s"] = df["bytesPerNano"];
 
-# g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
 
-g = sns.barplot(x="mb", y="gb/s", hue="method", data=df[df.bytes > 131072])
 
-g.set(ylim=(df[df.bytes > 131072]["gb/s"].min() - 1,df[df.bytes > 131072]["gb/s"].max() + 1))
+#
+#hmd_t1 = pd.DataFrame(df[(df["method"] == "kernel_transpose_multi_batch")])
+#hmd_t1.block_count = pd.to_numeric(hmd_t1.block_count)
+#hmd_t1.copy_count = pd.to_numeric(hmd_t1.copy_count)
+#hmd_t1.drop(columns=["machine", "mb", "fieldIDcount", "bytesPerNano", "bytes", "elemcount", "block_size"])
+#hmd_t1 = pd.pivot_table(hmd_t1, index="block_count", columns="copy_count", values="gb/s")
+#
+#ax = sns.heatmap(hmd_t1, annot=True)
+#
+#ax.set_title("kernel_trans1_multi_batch")
+#plt.show()
+#
+#
+#hmd_t2 = pd.DataFrame(df[(df["method"] == "kernel_trans2_multi_batch")])
+#hmd_t2.block_count = pd.to_numeric(hmd_t2.block_count)
+#hmd_t2.copy_count = pd.to_numeric(hmd_t2.copy_count)
+#hmd_t2.drop(columns=["machine", "mb", "fieldIDcount", "bytesPerNano", "bytes", "elemcount", "block_size"])
+#hmd_t2 = pd.pivot_table(hmd_t2, index="block_count", columns="copy_count", values="gb/s")
+#
+#ax = sns.heatmap(hmd_t2, annot=True)
+#
+#ax.set_title("kernel_trans2_multi_batch")
+#plt.show()
 
-# g.set_xticklabels(rotation=45)
+
+df["trans_method"] = df["method"] + df["copy_count"]
+
+scd = pd.DataFrame(df)
+scd = scd[(scd["copy_count"] == "8") | (scd["copy_count"] == "na")]
+scd["method"] = scd["method"] + "_cc_" + scd["copy_count"]
+scd2 = pd.DataFrame(df)
+scd2 = scd2[(scd2["method"] != "kernel_trans2_multi_batch") & (scd2["method"] != "kernel_transpose_multi_batch")]
+
+data = pd.concat([scd, scd2])
+
+
+g = sns.catplot("block_count", col="fieldIDcount", y="gb/s", hue="method", kind="point", col_wrap=1,  data=data)
+
+#g = sns.barplot(x="block_count", y="gb/s", hue="method", data=df[df.bytes > 131072])
+
+#g.set(ylim=(df[df.bytes > 131072]["gb/s"].min() - 1,df[df.bytes > 131072]["gb/s"].max() + 1))
+
+g.set_xticklabels(rotation=45)
 # g.fig.get_axes()[0].set_yscale('log')
 plt.show()
+
+
+
+
+
 
 # dfs = []
 
@@ -48,7 +91,7 @@ plt.show()
 # df["mb"] = round(df["bytes"]/1e+6,3); 
 # df["gb/s"] = df["bytesPerNano"]; 
 
-# g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="bar", col_wrap=1,  data=df)
+# g = sns.catplot("mb", col="fieldIDcount", y="gb/s", hue="method", kind="line", col_wrap=1,  data=df)
 
 # g.set_xticklabels(rotation=45)
 # g.fig.get_axes()[0].set_yscale('log')
